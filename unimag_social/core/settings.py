9fff7ee96ce51af7e5e94737d29f67e206c1a5bb
samir bertel <samir@kali.lenovo>
2026-02-19 18:43:50 -0500

    arreglos en settings

--- unimag_social/core/settings.py
+++ unimag_social/core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',  # Para la API
+    'rest_framework',  
+    'rest_framework.authtoken',
     'comunidad',
 ]
 
