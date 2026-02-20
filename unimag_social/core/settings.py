98fa0d324996fb7e56a993aabb889db187c671eb
samir bertel <samir@kali.lenovo>
2026-02-19 19:23:20 -0500

    arreglos en settings

--- unimag_social/core/settings.py
+++ unimag_social/core/settings.py
@@ -45,6 +45,16 @@ INSTALLED_APPS = [
     'comunidad',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
